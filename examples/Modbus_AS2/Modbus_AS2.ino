cddc33dc3cc1905446bc76c441028535f88f7ced
Mateus Dias <ma.domingues.dias@gmail.com>
2020-06-05 18:17:20 -0300

    modified

--- examples/Modbus_AS2/Modbus_AS2.ino
+++ examples/Modbus_AS2/Modbus_AS2.ino
@@ -1,54 +1,24 @@
-/////////////////////////////////////
-/*
-  Autor: Thellyson
-  Data: 04/03/2020
-*/
-/////////////////////////////////////
+#include <ModbusMaster232.h>
 
 #define TX_ENABLE_PIN 4
 
-////////////////////////////////////////////////////////////////////////////////
-//DECLARACOES DE BIBLIOTECAS
-////////////////////////////////////////////////////////////////////////////////
-#include <ModbusMaster232.h>    //ModbusMaster RTU pins   D7(13),D8(15)   RX,TX
+long tempo_anterior = 0;
 
-/////////////////////////////////////////////////////////////////////////////////
-//MODBUS
-/////////////////////////////////////////////////////////////////////////////////
-
-//Declaracoes do modbus:
-ModbusMaster232 node(1, TX_ENABLE_PIN);      //Buscando no slave id=1
-#define BAUD_RATE_MODBUS 9600                //BaudRate
-
+ModbusMaster232 node(1, TX_ENABLE_PIN);  //Slave ID 01
 
 void setup() {
-  Serial.begin(9600);
-
-  //Setup inicial do modbus:
-  //////////////////////////
-  node.begin(BAUD_RATE_MODBUS);
+  Serial.begin(115200);
+  node.begin();
 }
 
 void loop() {
-  //////////////////////////////////////////////////////////////////
-  //MODBUS
-  /////////////////////////////////////////////////////////////////
+  if (millis() - tempo_anterior > 5000) {
+    tempo_anterior = millis();
+    read_modbus();
+  }
+}
 
-  //  //Registro [1]
-  //  node.readHoldingRegisters(1, 1);
-  //  Serial.println("REG[1]");
-  //  delay(120);
-  //  Serial.println(node.getResponseBuffer(0));
-  //  node.clearResponseBuffer();
-
-  //Registro [1]
+void read_modbus() {
   node.readInputRegisters(1, 1);
-  Serial.println("REG[1]");
-  delay(120);
   Serial.println(node.getResponseBuffer(0));
-  delay(120);
-  node.clearResponseBuffer();
-  Serial.flush();
-//  Serial2.flush();
-  delay(1000);
 }
